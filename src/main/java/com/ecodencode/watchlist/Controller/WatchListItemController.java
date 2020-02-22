1c1f765be6ad85fb5be274a37e640375ee4a1e46
neorusse <neorusse@gmail.com>
2020-02-22 08:32:44 +0100

    chore: Add @valid annotation & bindingResult logic

--- src/main/java/com/ecodencode/watchlist/Controller/WatchListItemController.java
+++ src/main/java/com/ecodencode/watchlist/Controller/WatchListItemController.java
@@ -2,12 +2,14 @@ package com.ecodencode.watchlist.Controller;
 
 import com.ecodencode.watchlist.Model.WatchlistItem;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -64,7 +66,11 @@ public class WatchListItemController {
 
   // POST - Handles Watch List form submission for creating new item and updating an item
   @PostMapping("/watchlistItemForm")
-  public ModelAndView submitWatchlistItemForm(WatchlistItem watchlistItem) {
+  public ModelAndView submitWatchlistItemForm(@Valid WatchlistItem watchlistItem, BindingResult bindingResult) {
+
+    if (bindingResult.hasErrors()) {
+      return new ModelAndView("watchlistItemForm");
+    }
 
     WatchlistItem existingItem = findWatchlistItemById(watchlistItem.getId());
 
