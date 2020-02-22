e68d78c5e694ee00f8c5af8b3b081dd04fc3ddc1
neorusse <neorusse@gmail.com>
2020-02-22 08:38:34 +0100

    test: Write test for routes

---
+++ src/test/java/com/ecodencode/watchlist/WatchlistControllerTest.java
@@ -0,0 +1,52 @@
+package com.ecodencode.watchlist;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+@RunWith(SpringRunner.class)
+public class WatchlistControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  // unit testing get watch list route
+  @Test
+  public void testGetWatchlist() throws Exception {
+
+    mockMvc.perform(get("/watchlist"))
+      .andExpect(status().is2xxSuccessful())
+      .andExpect(view().name("watchlist"))
+      .andExpect(model().attributeExists("watchlistItems"))
+      .andExpect(model().attributeExists("numberOfMovies"));
+  }
+
+  // unit testing show watch list item form route
+  @Test
+  public void testShowWatchlistItemForm() throws Exception {
+
+    mockMvc.perform(get("/watchlistItemForm"))
+      .andExpect(status().is2xxSuccessful())
+      .andExpect(view().name("watchlistItemForm"))
+      .andExpect(model().size(1))
+      .andExpect(model().attributeExists("watchlistItem"));
+  }
+
+  @Test
+  public void testSubmitWatchlistItemForm() throws Exception {
+    mockMvc.perform(post("/watchlistItemForm"))
+      .andExpect(status().is3xxRedirection())
+      .andExpect(redirectedUrl("/watchlist"));
+  }
+}
