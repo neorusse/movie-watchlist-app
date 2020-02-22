4bb3055cbbaf23c326e99db9fe44cf53ccf549ec
neorusse <neorusse@gmail.com>
2020-02-22 21:50:59 +0100

    chore: Create data access layers for services

---
+++ src/main/java/com/ecodencode/watchlist/repository/WatchlistRepository.java
@@ -0,0 +1,45 @@
+package com.ecodencode.watchlist.repository;
+
+import com.ecodencode.watchlist.model.WatchlistItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WatchlistRepository {
+
+  private List<WatchlistItem> watchlistItems = new ArrayList<WatchlistItem>();
+  private static int index = 1;
+
+  // get all movies
+  public List<WatchlistItem> getList(){
+    return watchlistItems;
+  }
+
+  // add new movie
+  public void addItem(WatchlistItem watchlistItem) {
+    watchlistItem.setId(index++);
+    watchlistItems.add(watchlistItem);
+  }
+
+  // get a specific movie by Id
+  public WatchlistItem findById(Integer id) {
+    for (WatchlistItem watchlistItem : watchlistItems) {
+      if (watchlistItem.getId().equals(id)) {
+        return watchlistItem;
+      }
+    }
+    return null;
+  }
+
+  // get a specific movie by title
+  public WatchlistItem findByTitle(String title) {
+    for (WatchlistItem watchlistItem : watchlistItems) {
+      if (watchlistItem.getTitle().equals(title)) {
+        return watchlistItem;
+      }
+    }
+    return null;
+  }
+
+}
+
