ee591d30dd694319867ea23a5683107a770d6880
neorusse <neorusse@gmail.com>
2020-02-22 08:47:32 +0100

    chore: Create validator logic for @Rating annotation

---
+++ src/main/java/com/ecodencode/watchlist/RatingValidator.java
@@ -0,0 +1,24 @@
+package com.ecodencode.watchlist;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class RatingValidator implements ConstraintValidator<Rating, String> {
+
+  @Override
+  public boolean isValid(String value, ConstraintValidatorContext context) {
+
+    Double number;
+    try {
+      number = Double.parseDouble(value);
+    } catch (NumberFormatException e) {
+      return false;
+    }
+
+    if (number > 10 || number < 1) {
+      return false;
+    }
+
+    return true;
+  }
+}
