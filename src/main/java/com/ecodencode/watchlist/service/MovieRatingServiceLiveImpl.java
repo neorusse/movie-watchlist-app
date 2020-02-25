b2e729eeb36a8cbb79d738fd544434b94454bd7d
neorusse <neorusse@gmail.com>
2020-02-25 16:09:27 +0100

    chore: Retrieve OMDb API rating for movie

--- src/main/java/com/ecodencode/watchlist/service/MovieRatingServiceLiveImpl.java
+++ src/main/java/com/ecodencode/watchlist/service/MovieRatingServiceLiveImpl.java
@@ -1,5 +1,7 @@
 package com.ecodencode.watchlist.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,9 +11,11 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @ConditionalOnProperty(name = "app.environment", havingValue = "prod")
 @Service
-public class MovieRatingServiceImpl implements MovieRatingService {
+public class MovieRatingServiceLiveImpl implements MovieRatingService {
 
   String apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=8abdb146&t=";
+
+  private final Logger logger = LoggerFactory.getLogger(MovieRatingServiceLiveImpl.class);
 
   @Override
   public String getMovieRating(String title) {
@@ -20,17 +24,21 @@ public class MovieRatingServiceImpl implements MovieRatingService {
       // RestTemplate is as a HTTP client, just like a browser but inside our code.
       RestTemplate template = new RestTemplate();
 
+      logger.info("Calling OMDb API with url: {}", apiUrl + title);
+
       // using Spring’s RestTemplate (like a browser)  helper class to hit the API URL
       ResponseEntity<ObjectNode> response = template.getForEntity(apiUrl + title, ObjectNode.class);
 
       // get the body of the response
       ObjectNode jsonObject = response.getBody();
 
+      logger.debug("OMDb API response: {}", jsonObject);
+
       // extract the movie rating as text and return it.
       return jsonObject.path("imdbRating").asText();
 
     } catch (Exception e) {
-      System.out.println("Something went wrong while calling OMDb API" + e.getMessage());
+      logger.warn("Something went wrong while calling OMDb API" + e.getMessage());
       return null;
     }
   }
