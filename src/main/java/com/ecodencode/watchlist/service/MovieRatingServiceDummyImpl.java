f08563c74d62f41a0cc36cd89179055f96f9f960
neorusse <neorusse@gmail.com>
2020-02-25 16:02:19 +0100

    chore: MovieRatingService interface implementation

---
+++ src/main/java/com/ecodencode/watchlist/service/MovieRatingServiceDummyImpl.java
@@ -0,0 +1,15 @@
+package com.ecodencode.watchlist.service;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+@ConditionalOnProperty(name = "app.environment", havingValue = "dev")
+@Service
+public class MovieRatingServiceDummyImpl implements MovieRatingService {
+
+  @Override
+  public String getMovieRating(String title) {
+
+    return "9.99";
+  }
+}
